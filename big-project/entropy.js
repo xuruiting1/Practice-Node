d5560db6e09ea1b203550ef0a1d5f26793d295a4
wangding <408542507@qq.com>
2018-11-04 13:23:27 +0800

    entropy

--- big-project/entropy.js
+++ big-project/entropy.js
@@ -5,11 +5,33 @@ var arr = [],
     sum = 0;
 const log=console.log;
 
+function judge(value){
+  if(isNaN(value)){log('请输入数字 ！');return false;}
+  else if(value<0){log('请输入大于0的数 ！');return false;}
+  else if(value>1){log('请输入小于1的数！');return false;}
+  return true;
+}
+
+function sumjudge(){
+    sum = 0;
+    arr.forEach((key)=>{sum+=Number(key);});
+    if(Number(sum.toFixed(2)) < 1){console.log('命令行参数错误：概率空间不封闭！');
+    arr = [];input();return false;}
+    if(Number(sum.toFixed(2)) > 1){
+      log('概率和超过了 1！');arr = [];input();return false;
+    }
+    return true;
+}
+
+
 function calculate(){
     console.log('p = ',arr);
     hp = 0;
     arr.forEach((value)=>{hp += (-1)*value*Math.log2(value);});
+    //Math.log2(x)
+    //如果 x 为 NaN，则此函数返回 NaN。如果 x 小于零 (0)，则此函数返回 NaN
     console.log('\nH(p) = '+ hp.toFixed(3) +'bit');
+    //toFixed() 方法可把 Number 四舍五入为指定小数位数的数字
 }
 
 function input(){
@@ -18,6 +40,8 @@ function input(){
     log();
     log('p' + i + ':');
     process.stdin.on('data',(data)=>{
+      //一个指向标准输出流(stdout)的 可写的流(Writable Stream)：
+      //一个指向 标准输入流(stdin) 的可读流(Readable Stream)
       arr[i] = data.slice(0,data.length-1) - 0;
       if(judge(arr[i]) === true){
         i++;
@@ -39,23 +63,6 @@ function input(){
 });
 }
 
-function judge(value){
-  if(isNaN(value)){log('请输入数字 ！');return false;}
-  else if(value<0){log('请输入大于0的数 ！');return false;}
-  else if(value>1){log('请输入小于1的数！');return false;}
-  return true;
-}
-
-function sumjudge(){
-    sum = 0;
-    arr.forEach((key)=>{sum+=Number(key);});
-    if(Number(sum.toFixed(2)) < 1){console.log('命令行参数错误：概率空间不封闭！');
-    arr = [];input();return false;}
-    if(Number(sum.toFixed(2)) > 1){
-      log('概率和超过了 1！');arr = [];input();return false;
-    }
-    return true;
-}
 function argv(){
     if(process.argv.length === 2){input();}
     else if(process.argv.length === 3){

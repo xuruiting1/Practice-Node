ac9f85148538f497ea7c4e352f68d5e6e791ff34
wangding <408542507@qq.com>
2018-11-15 19:18:23 +0800

    http-server

--- 16-http-server/06-form-post.js
+++ 16-http-server/06-form-post.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 
+/*
+ * 自己写的，总是有一个错误，那就是第一次的时候多一个空白的
 const http=require('http');
 const log=console.log,
       url=require('url'),
@@ -15,17 +17,19 @@ http.createServer((req,res)=>{
     log(req.headers);
     log();
   }
-
+  
     if(req.url==='/'&&req.method==='GET'){
       showPage(req,res);
     }else if(req.url ==='/'&& req.method==='POST'){
       var data='';
       req.on('data',(chunk)=>{data += chunk;});
       req.on('end',()=>{
+                
         var list=qs.parse(data);
 
         log('list:',list);
         if(list.item !='') items.push(list.item);
+        
       });
       showPage(req,res);
     }else{
@@ -46,9 +50,9 @@ function showPage(req,res) {
               + '  <body>\n'
               + '    <h1>Todo List</h1>\n'
               + '    <ul>\n'
-              + items.map(function(item) {return '      <li>' + item + '</li>';}).join('\n')
+              + items.map(function(item) {return '<li>' + item + '</li>';}).join('\n')
               + '    </ul>\n'
-              + '    <form method="POST" action="/">\n'
+              + '    <form method="post" action="/">\n'
               + '       <p><input type="text" name="item" />'
               + '       <input type="submit" value="Add Item" /></p>\n'
               + '    </form>\n'
@@ -61,3 +65,82 @@ function showPage(req,res) {
       res.end(html);
 
 }
+*/
+
+const http = require('http'),
+      qs   = require('querystring');
+
+var items = [];
+
+http.createServer((req, res) => {
+  if(req.url != '/') {
+    err(res);
+    return;
+              
+  }
+  console.log(req.headers);
+  console.log('');
+  switch(req.method) {
+    case 'GET':
+      show(res);
+      break;
+    case 'POST':
+      add(req, res);
+      break;
+
+     default:
+       err(res);
+       break;                                               
+      }
+
+}).listen(8080);
+
+function show(res) {
+    var html = '<!DOCTYPE html>\n'
+      + '<html>\n'
+      + '  <head>\n'
+      + '    <meta charset="UTF-8">\n'
+      + '    <title>Todo list</title>\n'
+      + '  </head>\n'
+      + '  <body>\n'
+      + '    <h1>Todo List</h1>\n'
+                                                                               + '    <ul>\n'
+      + items.map(function(item) {return '      <li>' + item + '</li>';}).join('\n')
+                                                                                + '    </ul>\n'
+                                                                                + '    <form method="post" action="/">\n'
+                                                                                + '       <p><input type="text" name="item" />'
+       + '       <input type="submit" value="Add Item" /></p>\n'
+       + '    </form>\n'
+       + '  </body>\n'
+       + '</html>';
+
+      res.setHeader('Content-Type', 'text/html');
+      res.setHeader('Content-Length', Buffer.byteLength(html));
+      res.statusCode = 200;
+      res.end(html);
+}
+
+function add(req, res) {
+    var body = '';
+
+      req.on('data', function(chunk) { body += chunk;  });
+      req.on('end', function() {
+            console.log(body); 
+            if(body != '') {
+              items.push(qs.parse(body).item);           
+            }
+            show(res);        
+      });
+
+}
+
+function err(res) {
+    var msg = 'Not found!';
+
+    res.statusCode = 404;
+    res.setHeader('Content-Length', msg.length);
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(msg);
+
+}
+
